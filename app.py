4cf15b42b2f96e8e002006b75a13af643dcb636b
Ostrolutskyi Roman <zaoooza101@gmail.com>
2022-08-14 18:56:37 +0300

    prealpha 0.0.1

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask import request
+import sqlite3 as db
+
 app = Flask(__name__)
 
 
-@app.route("/currency/<value_name>/review", methods=['GET', 'POST', 'PUT', 'DELETE'])
-def feedback():
+def get_data(query: str):
+    con = db.connect('exchange.db')
+    cursor = con.execute(query)
+    result = cursor.fetchall()
+    con.close()
+    return result
+
+
+@app.route("/Currency/<value_name>/rating", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def feedback(value_name):
     if request.method == 'GET':
-        pass
+        response_db = get_data(
+            f'select round(avg(rating), 2), cur_name from Rating where cur_name="{value_name}"')
+        return response_db
     elif request.method == 'POST':
         pass
     elif request.method == 'PUT':
@@ -15,52 +27,58 @@ def feedback():
         pass
 
 
-@app.get("/currency/<value_name>")
-def show_currency():
-    pass
+@app.get("/Currency/<value_name>")
+def show_currency(value_name):
+    response_db = get_data(f'select * from Currency where currency_name = "{value_name}"')
+    return response_db
 
 
-@app.route("/currency/trade/<value>/<second_value>", methods=['GET', 'POST'])
-def exchange():
+@app.route("/Currency/trade/<value>/<second_value>", methods=['GET', 'POST'])
+def exchange(value, second_value):
     if request.method == 'GET':
-        pass
+        response_db = get_data(f'''select round(
+        (select cost_concerning_USD from Currency where datatime="11.08.2021" and currency_name="{value}")/
+        (select cost_concerning_USD from Currency where datatime="11.08.2021" and currency_name="{second_value}"), 2)
+''')
+        return response_db
     else:
         pass
 
 
-@app.get("/currency")
+@app.get("/Currency")
 def home_page():
-    pass
+    response_db = get_data(f' select * from Currency')
+    return response_db
 
 
-@app.get("/user")
-def user_page():
-    pass
+@app.get("/User/<user_id>")
+def user_page(user_id):
+    response_db = get_data(f'select User.login, Account.currency_name, Account.balance, Account.user_deposit, '
+                           f'Account.currency_id from User join Account where User.id=Account.user_id = {user_id}')
+    return response_db
 
 
-@app.post("/user/transfer")
+@app.post("/User/transfer")
 def user_transfer():
     pass
 
 
-@app.get("/user/history")
-def user_histroy():
-    pass
+@app.get("/User/history/<user_name>")
+def user_history(user_name):
+    response_db = get_data(f'select history from History where user_name ="{user_name}"')
+    return response_db
 
 
-@app.route("/user/deposit", methods=['GET', 'POST'])
-def user_deposit():
+@app.route("/User/deposit/<deposit_id>", methods=['GET', 'POST'])
+def user_deposit(deposit_id):
     if request.method == 'GET':
-        pass
+        response_db = get_data(f'select user_deposit from Account where user_deposit="{deposit_id}" ')
+        return response_db
     else:
         pass
 
 
-@app.get("/user/deposit/<deposit_id>")
-def deposit():
-    pass
-
-
-@app.get("/deposit/<value_name>")
-def choose_deposit():
-    pass
+@app.get("/Deposit/<deposit_value>")
+def choose_deposit(deposit_value):
+    response_db = get_data(f'select * from Deposit where value_name="{deposit_value}"')
+    return response_db
